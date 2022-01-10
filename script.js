aa6395369b59314ac51df97140319ca88fd6985b
its-codetime <itscodetiime@gmail.com>
2022-01-10 11:49:27 +0530

    .

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ form.addEventListener("submit", (e) => {
   e.preventDefault();
   const country = input.value;
   const option = document.createElement("option");
-  option.textContent = country;
+  option.innerText = country;
   option.value = country;
   console.log({ country, option });
   selectElement.appendChild(option);
