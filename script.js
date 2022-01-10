ce64ca2037959bf593c4768e0611e7bc6e17b5bf
its-codetime <itscodetiime@gmail.com>
2022-01-10 12:19:01 +0530

    select colors

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// task 1
 const selectElement = document.querySelector("#countries");
 const form = document.querySelector("form");
 const input = document.querySelector("#input");
@@ -5,10 +6,21 @@ const input = document.querySelector("#input");
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   const country = input.value;
+  if (country.length < 1) return;
   const option = document.createElement("option");
   option.innerText = country;
   option.value = country;
   console.log({ country, option });
   selectElement.appendChild(option);
   input.value = "";
+});
+
+// task-2
+const colors = Array.from(document.querySelectorAll(".color"));
+const output = document.querySelector(".output");
+
+colors.forEach((color) => {
+  color.addEventListener("click", () => {
+    output.innerText = color.dataset.color;
+  });
 });
